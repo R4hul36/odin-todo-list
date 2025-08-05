ac069120acd4ec347f35894325e82ed51bcfaf68
Rahul <rahul.wa47@gmail.com>
2025-08-05 12:08:47 +0530

    added function to update/edit todo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css'
 import { createTodo } from './modules/todo'
-import { removeTodo } from './modules/todoManger'
+import { removeTodo, updateTodo } from './modules/todoManger'
 
 console.log('hellow world')
 
@@ -10,5 +10,7 @@ const myTodo = createTodo('walking', 'walk today evening at 4:00 pm', 'medium')
 todos.push(myTodo)
 console.log(todos);
 
-todos = removeTodo(todos, todos[0].id)
+todos = updateTodo(todos, todos[0].id, createTodo("Running", "Run for 30 minutes", "important"))
+
+// todos = removeTodo(todos, todos[0].id)
 console.log(todos);
--- src/modules/todoManger.js
+++ src/modules/todoManger.js
@@ -3,3 +3,15 @@
 export const removeTodo = function (todos, id) {
     return todos.filter((todo) => todo.id !== id)
 }
+
+export const updateTodo = function (todos, id, todo) {
+    const updatedTodo = todos.map((t) => {
+        if(t.id === id) {
+            return todo;
+        }
+        return t;
+    })
+    
+    
+    return updatedTodo;
+}
