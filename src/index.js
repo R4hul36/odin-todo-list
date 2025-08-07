6942d085bf8be732313e12909d48c806032d4522
Rahul <rahul.wa47@gmail.com>
2025-08-07 11:38:35 +0530

    added dialog to html and minimal initialization code in JS

--- src/index.js
+++ src/index.js
@@ -5,24 +5,34 @@ import { removeTodo, updateTodo } from './modules/todoManger'
 
 console.log('hellow world')
 
-let todos = []
-let projects = []
+// let todos = []
+// let projects = []
 
-const myTodo = createTodo(
-  'walking',
-  'walk today evening at 4:00 pm',
-  'medium',
-  '2025, 8, 7'
-)
-todos.push(myTodo)
-console.log(todos)
+// const myTodo = createTodo(
+//   'walking',
+//   'walk today evening at 4:00 pm',
+//   'medium',
+//   '2025, 8, 7'
+// )
+// todos.push(myTodo)
+// console.log(todos)
 
-todos = updateTodo(
-  todos,
-  todos[0].id,
-  createTodo('Running', 'Run for 30 minutes', 'important', '2025, 8, 11')
-)
+// todos = updateTodo(
+//   todos,
+//   todos[0].id,
+//   createTodo('Running', 'Run for 30 minutes', 'important', '2025, 8, 11')
+// )
 
-const myProject = createProject('study Math', '2 hours of math')
-projects.push(myProject)
-console.log(projects)
+// const myProject = createProject('study Math', '2 hours of math')
+// projects.push(myProject)
+// console.log(projects)
+
+const taskButton = document.querySelector("#add-todo");
+const projectButton = document.querySelector("#add-project");
+const taskDialog = document.querySelector("#todo-dialog");
+const projectDialog = document.querySelector("#project-dialog")
+
+
+taskButton.addEventListener("click", (e) => {
+  taskDialog.showModal()
+})
